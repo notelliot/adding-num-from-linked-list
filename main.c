dfdbd0bf7ee32dd150c8fee991a23937780a2d9c
notelliot <notelliot1234@gmail.com>
2024-06-22 05:27:49 +0100

    adding-num-from-linked-list

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/* You are given two empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself. 
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: 807
+Explanation: 342 + 465 = 807
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
